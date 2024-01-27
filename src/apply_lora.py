6f213d829220c75d61c0ab50edec095dcf6897ba
Paul Ilioaica <38083420+paulilioaica@users.noreply.github.com>
2024-01-27 21:30:54 +0200

    Update README and notebook

--- src/apply_lora.py
+++ src/apply_lora.py
@@ -1,26 +1,35 @@
-from lora import LoRADecorator
+from lora_decorator import LoRADecorator_Linear
 import torch.nn as nn
 from functools import reduce
-
-
-# traverse the model, print all the trainable layer names
-# do it recursively
-from functools import reduce  
+from copy import deepcopy
 
 
 lora_to_replace = ['linear', 'out_proj']
 
-def apply_lora(model, rank, alpha):  
+def apply_lora(model_reference, rank, alpha):  
+    
+    # copy so we don't keep a reference to the original model
+    model = deepcopy(model_reference)
+
+    # first thing first, freeze all the layers
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # then, replace all the linear layers with LoRADecorator_Linear
     for name, layer in model.named_modules():  
         if len(list(layer.children())) == 0:  
+
             name_child = name.split('.')[-1] 
+            
             if name_child[-1].isdigit():
                 name_child = name_child[:-1]
-            if name_child  in lora_to_replace and not isinstance(layer, LoRADecorator):
-                new_layer = LoRADecorator(rank, layer.in_features, layer.out_features, alpha)  
+
+            if name_child  in lora_to_replace and not isinstance(layer, LoRADecorator_Linear):
+                new_layer = LoRADecorator_Linear(layer, rank, alpha)  
                 # get the parent module  
                 parent_name, child_name = name.rsplit('.', 1)  
                 parent_module = reduce(getattr, parent_name.split('.'), model)  
                 # replace the layer in the parent module  
                 parent_module._modules[child_name] = new_layer  
+
     return model  
--- src/lora_layer.py
+++ src/lora_layer.py
@@ -2,13 +2,14 @@ import torch.nn as nn
 import torch
 
 
-
-
-class LoRA(nn.Module):
+class LoRALayer(nn.Module):
     def __init__(self, rank, hidden_size_in, hidden_size_out, alpha):
         super().__init__()
         self.rank = rank
         self.alpha = alpha
+
+        self.in_features = hidden_size_in
+        self.out_features = hidden_size_out
 
         self.std_dev = 1 / torch.sqrt(torch.tensor(rank).float())
         self.A = nn.Parameter(torch.randn(rank, hidden_size_out))
@@ -16,10 +17,20 @@ class LoRA(nn.Module):
 
     
     def forward(self, x):
-        
-        x = self.B @ self.A * x
-        
-        return x * self.alpha * 1/self.std_dev
 
+        if len(x.shape) == 3:
+            dim1, dim2, _ = x.shape
+            x = x.reshape(-1, self.in_features)
+            x = x @ self.B @ self.A
+            x = x.view(dim1, dim2, self.out_features)
 
+        elif len(x.shape) <= 2:
+            x = x @ self.B @ self.A
+
+        elif len(x.shape) > 3:
+            raise ValueError("LoRALayer only supports 2D or 3D inputs, got {}D".format(len(x.shape)))
         
+    
+
+        return x * self.alpha * 1 / self.std_dev
+
